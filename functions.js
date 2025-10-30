fb1a94eb5799788a7ec13bee2020e965d8fc9bfb
ShadaabIqbal <iqbalshadaab@gmail.com>
2025-10-30 20:38:16 +0530

    Done

---
+++ functions.js
@@ -0,0 +1,65 @@
+// FUNCTIONS IN JAVASCRIPT:
+
+// SYNTAX: function sum(){ //code };
+
+// function printHelloWorld(){
+//     console.log("Hello World");
+//     console.log("These are javascript functions");
+//     console.log("Funtions in js");
+// }
+
+// printHelloWorld();
+
+// The value that are passed in the function when calling a function
+// are called arguments and the values that we recieve in a function
+// are called parameters.
+// SYNTAX: function sum(parameters...){ //code };
+//         sum(arguments....)
+
+// function sum(a,b){
+//     // let sum = a+b;
+//     console.log(a);
+//     console.log(b);
+// }
+
+// sum(10, 20);
+
+// function greet(name) {
+//   console.log("Hello" + " " + name);
+// }
+
+// greet("Ayaan");
+
+// Return always ends the function.
+// Without return, the function gives undefined.
+
+// function sum(a, b, c) {
+//   let sum = a + b + c;
+//   return sum;
+// }
+
+// let sumOfDigits = sum(10, 30, 50);
+// console.log(sum(10, 20, 30));
+// console.log(sumOfDigits);
+
+// let a = 5.5;
+
+// let b = Math.ceil(a);
+
+// console.log(b);
+
+// Variables declared inside a function are not accessible outside it.
+
+// function sumOfArr(arr) {
+//   let sum = 0; 
+//   for (let i = 0; i < arr.length; i++) {
+//     sum = sum + arr[i];
+//   }
+//   return sum;
+// }
+
+
+// let arr = [4, 2, 5, 6, 9, 10];
+
+// let result = sumOfArr(arr);
+// console.log(result);
